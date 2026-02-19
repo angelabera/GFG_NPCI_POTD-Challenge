b72abf981c82dcb825e14edb375a0339e836c476
Angela Bera <angelaa.bera@gmail.com>
2026-02-19 18:29:52 +0530

    19th Feb Challenge

---
+++ MissingElementinRange.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+class Solution {
+    public ArrayList<Integer> missingRange(int[] arr, int low, int high) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        HashSet<Integer> set=new HashSet<>();
+        for(int i : arr) set.add(i);
+        
+        for(int i=low;i<=high;i++){
+            if(set.contains(i)==false) ans.add(i);
+        }
+        
+        return ans;
+    }
+}
