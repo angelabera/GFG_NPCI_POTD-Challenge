355679534ec8d52e1aec6cda3b732fcaf7e88655
Angela Bera <angelaa.bera@gmail.com>
2026-03-08 09:57:30 +0530

    8th March Challenge

---
+++ PythagoreanTriplet.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pythagoreanTriplet(self, arr):
+        MAX = 1000
+        present = [False]*(MAX+1)
+        for num in arr:
+            present[num] = True
+            
+        for a in range(1, MAX+1):
+            if not present[a]:
+                continue
+            
+            for b in range(a+1, MAX+1):
+                if not present[b]:
+                    continue
+                
+                squared = (a*a+b*b)
+                c = int(squared**0.5)
+                if c <= MAX and c*c == squared and present[c]:
+                    return True
+        return False
