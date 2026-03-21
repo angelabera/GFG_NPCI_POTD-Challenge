9debfca7cfbc920570228bfffafd0a2fc7273c80
Angela Bera <angelaa.bera@gmail.com>
2026-03-21 09:20:44 +0530

    20th March Challenge

---
+++ Predeccessor&Successor.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findPreSuc(self, root, key):
+        pred = None
+        succ = None
+        
+        curr = root
+        
+        while curr:
+            if curr.data < key:
+                pred = curr
+                curr = curr.right
+            elif curr.data > key:
+                succ = curr
+                curr = curr.left
+            else:
+                # predecessor = max in left subtree
+                if curr.left:
+                    temp = curr.left
+                    while temp.right:
+                        temp = temp.right
+                    pred = temp
+                
+                # successor = min in right subtree
+                if curr.right:
+                    temp = curr.right
+                    while temp.left:
+                        temp = temp.left
+                    succ = temp
+                break
+        
+        return pred, succ 
