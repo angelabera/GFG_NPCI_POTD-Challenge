7f6442b9faa1e9d50e9bd00b1605d708ea50c9a7
Angela Bera <angelaa.bera@gmail.com>
2026-04-08 09:29:42 +0530

    8th April Challenge

---
+++ Segregate0s&1s.py
@@ -0,0 +1,7 @@
+class Solution:
+    def segregate0and1(self, arr):
+        i = 0
+        for j in range(len(arr)):
+            if arr[j] == 0:
+                arr[i], arr[j] = arr[j], arr[i]
+                i += 1
